8d44b0324c5ffaa447f333109386f16931b9d900
vinicius-robles <vinicius.robles@gmail.com>
2025-03-23 18:20:44 -0300

    Adicionado código completo de aplicação que faz simulação de banho de pets em uma máquina.

---
+++ exercicios-classes-e-encapsulamento/src/petshop/Main.java
@@ -0,0 +1,82 @@
+package petshop;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private final static PetMachine petMachine = new PetMachine();
+
+    public static void main(String[] args) {
+        scanner.useDelimiter("\\n");
+        var option = -1;
+
+            do {
+                System.out.println("===Escolha uma das opções===");
+                System.out.println("1 - Dar banho no pet");
+                System.out.println("2 - Abastecer máquina com água");
+                System.out.println("3 - Abastecer máquina com shampoo");
+                System.out.println("4 - Verificar água da máquina");
+                System.out.println("5 - Verificar shampoo da máquina");
+                System.out.println("6 - Verificar se tem pet no banho");
+                System.out.println("7 - Colocar pet na máquina");
+                System.out.println("8 - Retirar pet da máquina");
+                System.out.println("9 - Limpar máquina");
+                System.out.println("0 - Sair");
+                option = scanner.nextInt();
+
+                switch (option){
+                    case 1 -> petMachine.takeAShower();
+                    case 2 -> setWater();
+                    case 3 -> setShampoo();
+                    case 4 -> verifyWater();
+                    case 5 -> verifyShampoo();
+                    case 6 -> checkIfHasPetInMachine();
+                    case 7 -> setPetInPetMachine();
+                    case 8 -> petMachine.removePet();
+                    case 9 -> petMachine.wash();
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Opção inválida");
+                }
+
+            } while(option != 0);
+    }
+
+    private static void setWater(){
+        System.out.println("Tentando colocar água na máquina");
+        petMachine.addWater();
+    }
+
+    private static void setShampoo(){
+        System.out.println("Tentando colocar shampoo na máquina");
+        petMachine.addShampoo();
+    }
+
+    private static void verifyWater() {
+        var amount = petMachine.getWater();
+        System.out.println("A máquina está no momento com " + amount + " litros de água");
+    }
+
+    private static void verifyShampoo() {
+        var amount = petMachine.getShampoo();
+        System.out.println("A máquina está no momento com " + amount + " litros de shampoo");
+    }
+
+    private static void checkIfHasPetInMachine() {
+        var hasPet = petMachine.hasPet();
+        System.out.println(hasPet ? "Tem pet na máquina" : "Não tem pet na máquina");
+    }
+
+    public static void setPetInPetMachine(){
+        var name = "";
+        while(name == null || name.isEmpty()){
+            System.out.println("Informe o nome do pet:");
+            name = scanner.next();
+        }
+
+        var pet = new Pet(name);
+        petMachine.setPet(pet);
+    }
+
+}
--- exercicios-classes-e-encapsulamento/src/petshop/Petshop.java
+++
@@ -1,9 +0,0 @@
-package petshop;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Petshop {
-    public static void main(String[] args) {
-
-    }
-}
